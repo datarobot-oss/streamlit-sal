cb3e3f7cf5d44f4c048578f3f6879162186d2359
kideh88 <5627401+kideh88@users.noreply.github.com>
2024-06-20 14:14:07 +0200

    [APP-2788] Change util functions to be LESS applied styles (#4)
    * [APP-2788] Change util functions to be LESS applied styles
    
    * [APP-2788] Fix example texts

--- example.py
+++ example.py
@@ -3,8 +3,12 @@ import streamlit_sal as sal
 from streamlit_sal import sal_stylesheet
 
 
-with sal_stylesheet():
+with sal_stylesheet(reduce_markdown_spacing=True, move_sidebar_right=True):
     st.header('Example app for SAL')
+    st.subheader('Remember to run `streamlit-sal compile` before running this example app')
 
     with sal.button():
-        st.button('Custom Styled button')
+        st.button('Custom styled button')
+
+    with st.sidebar:
+        st.write('The sidebar is now on the right side')
