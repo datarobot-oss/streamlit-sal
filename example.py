0989448503f9552c0b74342322020b5939df8dbc
Kim Dehmlow <5627401+kideh88@users.noreply.github.com>
2024-07-03 16:52:45 +0200

    [APP-2775] Add extensive README , fix small naming bug

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit_sal as sal
 from streamlit_sal import sal_stylesheet
 
-with sal_stylesheet(reduce_markdown_spacing=True, move_sidebar_right=True):
+with sal_stylesheet():
     st.header('Example app for SAL')
     st.subheader('Remember to run `streamlit-sal compile` before running this example app')
 
