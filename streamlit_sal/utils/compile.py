f6b9d9b5ed38d20bc699d1280dda21ed904c4e29
kideh88 <5627401+kideh88@users.noreply.github.com>
2024-07-03 08:59:23 +0200

    [APP-2865] Keep SASS util files private (#12)
    * [APP-2865] Keep SASS util files private
    
    * [APP-2865] Use include_paths instead of placeholders

--- streamlit_sal/utils/compile.py
+++ streamlit_sal/utils/compile.py
@@ -1,4 +1,5 @@
 import os
+from importlib.resources import files
 
 import sass
 
@@ -10,6 +11,7 @@ def run_compile():
     # Define the input SASS file
     sass_path = get_config_value(ConfigOptions.SASS_SOURCE_PATH.value)
     input_file = os.path.join(sass_path, STYLE_SASS_MAIN_FILE_NAME)
+    include_paths = [str(files(f"streamlit_sal.sass")), sass_path]
 
     # Define the output CSS file
     css_path = get_config_value(ConfigOptions.CSS_STYLESHEET_FILE_PATH.value)
@@ -17,6 +19,7 @@ def run_compile():
     output_file = os.path.join(css_path, css_file)
 
     with open(output_file, 'w') as f:
-        compiled_css = sass.compile(filename=input_file)  # TODO Consider using output_style='compressed' ?
+        compiled_css = sass.compile(filename=input_file,
+                                    include_paths=include_paths)  # TODO Consider using output_style='compressed' ?
         f.write(compiled_css)
         print(f"Compiled {input_file} to {output_file}")
